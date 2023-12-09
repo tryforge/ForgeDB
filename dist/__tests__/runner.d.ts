90aac3f3f590b6a008840eac57bd09b9ae761866
tryforge <aggeloss007@gmail.com>
2023-12-09 16:11:43 +0200

    forgor to remove my bot token

--- dist/__tests__/runner.d.ts
+++ dist/__tests__/runner.d.ts
@@ -1,4 +1,1 @@
-declare const ForgeClient: any;
-declare const ForgeDB: any;
-declare const client: any;
 //# sourceMappingURL=runner.d.ts.map
--- dist/__tests__/runner.js
+++ dist/__tests__/runner.js
@@ -1,26 +1,3 @@
 "use strict";
-const { ForgeClient } = require("forgescript");
-const { ForgeDB } = require("../index");
-const client = new ForgeClient({
-    intents: [
-        "GuildMessages",
-        "Guilds",
-        "MessageContent" // This intent is privileged, must be enabled in https://discord.com/developers/applications
-    ],
-    events: [
-        "messageCreate",
-        "ready"
-    ],
-    prefixes: [
-        "t!",
-        "?"
-    ],
-    extensions: [new ForgeDB()]
-});
-client.commands.add({
-    name: "e",
-    code: `$eval[$message]`,
-    type: "messageCreate" // The event to act on
-});
-client.login("MTE0NTAzNjYzODA2NjU4NTY1Mw.GgiaNg.KQQmJJpv8HbMXsRGANGnusLjXq8vnLZ2jzOc9o");
+console.log('test');
 //# sourceMappingURL=runner.js.map
