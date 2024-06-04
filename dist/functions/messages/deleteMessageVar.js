d8c4f180f7cd43c0b63e4c85c7a29327832b136c
aggelos-007 <aggeloss007@gmail.com>
2024-06-04 15:34:56 +0300

    Message vars

---
+++ dist/functions/messages/deleteMessageVar.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const util_1 = require("../../util");
+exports.default = new forgescript_1.NativeFunction({
+    name: "$deleteMessageVar",
+    version: "2.0.0",
+    description: "Deletes a value from a message variable",
+    unwrap: true,
+    brackets: true,
+    args: [
+        {
+            name: "name",
+            description: "The name of the variable",
+            rest: false,
+            type: forgescript_1.ArgType.String,
+            required: true,
+        }, {
+            name: "message ID",
+            description: "The identifier of the value",
+            rest: false,
+            type: forgescript_1.ArgType.Message,
+            required: false,
+        }
+    ],
+    async execute(ctx, [name, message]) {
+        await util_1.DataBase.delete({ name, id: message?.id ?? ctx.message.id, type: "message" });
+        return this.success();
+    },
+});
+//# sourceMappingURL=deleteMessageVar.js.map
---
+++ dist/functions/messages/getMessageVar.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const forgescript_1 = require("@tryforge/forgescript");
+const util_1 = require("../../util");
+const __1 = require("../..");
+exports.default = new forgescript_1.NativeFunction({
+    name: "$getMessageVar",
+    version: "2.0.0",
+    description: "Returns a variable's value of a message",
+    output: forgescript_1.ArgType.Unknown,
+    unwrap: true,
+    args: [
+        {
+            name: "name",
+            description: "The name of the variable",
+            rest: false,
+            type: forgescript_1.ArgType.String,
+            required: true,
+        },
+        {
+            name: "message ID",
+            description: "The identifier of the value",
+            rest: false,
+            type: forgescript_1.ArgType.Message,
+            required: false,
+        },
+        {
+            name: "default",
+            description: "The default value if the identifier doesn't exist in the variable",
+            rest: false,
+            required: false,
+            type: forgescript_1.ArgType.String,
+        },
+    ],
+    brackets: true,
+    async execute(ctx, [name, message, def]) {
+        const data = await util_1.DataBase.get({ name, id: message?.id ?? ctx.message.id, type: "message" }).then((x) => x?.value);
+        if (data === null || data === undefined) {
+            if (def)
+                return this.successJSON(def);
+            else if (__1.ForgeDB.defaults && name in __1.ForgeDB.defaults) {
+                const defData = __1.ForgeDB.defaults[name];
+                if (typeof defData === "object" && defData !== null && "functions" in defData) {
+                    const d = defData;
+                    // Run
+                    const result = await forgescript_1.Interpreter.run(ctx.clone({
+                        data: d,
+                        allowTopLevelReturn: true,
+                        doNotSend: true,
+                        redirectErrorsToConsole: true,
+                    }));
+                    return result === null ? this.stop() : this.successJSON(result);
+                }
+                else
+                    return this.successJSON(defData);
+            }
+        }
+        return this.successJSON(data);
+    },
+});
+//# sourceMappingURL=getMessageVar.js.map
