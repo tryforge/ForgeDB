1f182c105e62ed6ad1c7268d288e6982ffd85432
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-06 19:29:19 +0530

    Update oldData.ts
    desc

--- src/functions/events/oldData.ts
+++ src/functions/events/oldData.ts
@@ -6,12 +6,12 @@ export enum DataType { identifier, name, id, type, value, guildId }
 export default new NativeFunction({
     name: "$oldData",
     version: "2.0.0",
-    description: "Retrieve the old data has been updated of a record on update event",
+    description: "Retrieves the old data that has been updated for a record during an update event",
     unwrap: true,
     args: [
         {
             name: "type",
-            description: "The type of what you want to retrieve",
+            description: "The type of data you want to retrieve",
             rest: false,
             type: ArgType.Enum,
             enum: DataType,
