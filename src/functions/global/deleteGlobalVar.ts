bbabb5c240537c5e9faa81addf59a085140a92c5
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-06 19:29:56 +0530

    Update deleteGlobalVar.ts
    desc

--- src/functions/global/deleteGlobalVar.ts
+++ src/functions/global/deleteGlobalVar.ts
@@ -4,13 +4,13 @@ import { DataBase } from "../../util"
 export default new NativeFunction({
     name: "$deleteGlobalVar",
     version: "2.0.0",
-    description: "Deletes a value from a global variable",
+    description: "Removes a value from a global variable",
     unwrap: true,
     brackets: true,
     args: [
         {
             name: "name",
-            description: "The name of the variable",
+            description: "The name of the variable from which to remove the value",
             rest: false,
             type: ArgType.String,
             required: true,
