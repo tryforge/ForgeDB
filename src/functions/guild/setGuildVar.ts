5e977e2dbf65343ccfe04844bb183b84ed48643b
Aditya <105863819+Clyders@users.noreply.github.com>
2024-06-06 19:42:05 +0530

    Update setGuildVar.ts
    desc

--- src/functions/guild/setGuildVar.ts
+++ src/functions/guild/setGuildVar.ts
@@ -4,24 +4,26 @@ import { DataBase } from "../../util"
 export default new NativeFunction({
     name: "$setGuildVar",
     version: "2.0.0",
-    description: "Sets a guild's value in a variable",
+    description: "Assigns a value to a variable associated with a guild",
     unwrap: true,
     args: [
         {
             name: "name",
-            description: "The name of the variable",
+            description: "The name of the variable to set the value in",
             rest: false,
             type: ArgType.String,
             required: true,
-        },{
+        },
+        {
             name: "value",
-            description: "The value",
+            description: "The value to be assigned",
             rest: false,
             required: true,
             type: ArgType.String,
-        },{
+        },
+        {
             name: "guild ID",
-            description: "The guild id of the variable",
+            description: "The guild ID for which to set the variable value",
             rest: false,
             type: ArgType.Guild,
             required: false,
