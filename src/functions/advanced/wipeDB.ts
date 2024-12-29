9ec1cdb42d4819e0a0b5fc28cb976cdb342f74d6
aggelos-007 <aggeloss007@gmail.com>
2024-12-29 20:22:53 +0200

    added $wipeDB

---
+++ src/functions/advanced/wipeDB.ts
@@ -0,0 +1,16 @@
+import { ArgType, NativeFunction } from "@tryforge/forgescript"
+import { DataBase } from "../../util"
+
+export default new NativeFunction({
+    name: "$wipeDB",
+    version: "1.0.0",
+    aliases: ["$deleteDB", "$clearDB"],
+    description: "Whipes all the data stored in the database including cooldowns",
+    output: ArgType.Json,
+    unwrap: false,
+    async execute(_ctx) {
+        await DataBase.wipe();
+        await DataBase.cdWipe()
+        return this.success()
+    },
+})
